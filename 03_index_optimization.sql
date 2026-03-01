124377932f089f32f25468a726fcb0b3f17a4a19
ritaaaa-yry <ry127@duke.edu>
2026-03-01 12:20:41 +0800

    Add files via upload

---
+++ 03_index_optimization.sql
@@ -0,0 +1,14 @@
+﻿-- 03_index_optimization.sql
+-- Resume alignment #2: index optimization for heavy joins/sorts
+
+CREATE INDEX idx_exam_resident_date
+ON patients_physical_exam(resident_id, exam_date);
+
+CREATE INDEX idx_audience_resident_time
+ON audiences(resident_id, visit_time);
+
+CREATE INDEX idx_positive_case_resident_discharge
+ON positive_cases(resident_id, discharge_date);
+
+CREATE INDEX idx_feature_risk_score
+ON resident_features(risk_score);
